1d7dd898bffe3c1640d25a851930e51c6d7c99cd
AyeNyeinSan <aye2904@gmail.com>
2025-05-27 11:31:14 +0700

    Chore: style and draft for MLops

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,20 +1,18 @@
 export default function NotFoundPage() {
     return (
-      <>
-        <h1>404 Not Found</h1>
+      <div className="flex flex-col justify-center justify-items-center m-6">
+        <h1 className="text-4xl font-bold m-2">404 Not Found</h1>
         <p>You landed on a page that just doesn't work right now.</p>
-        <div style={{ width: '100%', height: 0, paddingBottom: '83%', position: 'relative' }}>
+        <div className="mt-8">
           <iframe
             src="https://giphy.com/embed/T1WqKkLY753dZghbu6"
-            width="100%"
-            height="100%"
-            style={{ position: 'absolute' }}
+            width="480"
+            height="398"
             frameBorder="0"
-            className="giphy-embed"
+            className="giphy-embed block mx-auto"
             allowFullScreen
           ></iframe>
         </div>
-       
-      </>
+      </div>
     );
 }
